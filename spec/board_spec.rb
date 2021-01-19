7d412b4b2665894f9aa4e1ff87148fded7e9811c
Adam Jonas <adamjonas25@gmail.com>
2021-01-19 21:13:06 +0000

    pass initialize spec for length of board grid row

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,6 +8,9 @@ module Mastermind
         expect(@board.grid.size).to eq 2
       end
 
+      it 'has four objects in each row' do
+        expect@board.grid[0].length
+      end
     end
   end
 end
