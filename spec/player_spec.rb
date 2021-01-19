ba04ae465830b0ebde81396a33c59c437112c2ce
Adam Jonas <adamjonas25@gmail.com>
2021-01-19 20:17:12 +0000

    pass spec for name getter method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,5 +5,11 @@ module Mastermind
         expect(Player.new('').class).to eq Player
       end
     end
+
+    context '#name' do
+      it 'returns the player name' do
+        expect(Player.new('name').name).to eq 'name'
+      end
+    end
   end
 end
