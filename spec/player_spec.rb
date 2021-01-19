c7008a548f2ed75773999060997f00d44e50fd95
Adam Jonas <adamjonas25@gmail.com>
2021-01-19 20:21:16 +0000

    pass test for setter method on player name

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,14 +1,21 @@
 module Mastermind
   describe Player do
+    before(:all) { @player = Player.new('name')}
+
     context '#initialize' do
       it 'creates a new instance of Player' do
-        expect(Player.new('').class).to eq Player
+        expect(@player.class).to eq Player
       end
     end
 
     context '#name' do
       it 'returns the player name' do
-        expect(Player.new('name').name).to eq 'name'
+        expect(@player.name).to eq 'name'
+      end
+
+      it 'can set the player name' do
+        @player.name = 'new name'
+        expect(@player.name).to eq 'new name'
       end
     end
   end
