081225754a4219312e9fc6636181b584a1f4384d
Adam Jonas <adamjonas25@gmail.com>
2021-01-19 21:10:47 +0000

    create board class, pass first initialize spec

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,13 @@
-# module Mastermind
-#   class Board
-#     attr_accessor :grid, :code, :guess
-#     def initialize()
-#       @grid =
-#       @code =
-#       @guess =
-#   end
-#
-#   def defaults
-#     grid =
-# end
+module Mastermind
+  class Board
+    attr_accessor :grid
+
+    def initialize(grid = default_grid)
+      @grid = grid
+    end
+
+    def default_grid
+      Array.new(2) { Array.new(4) { Cell.new } }
+    end
+  end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,12 +2,12 @@ module Mastermind
   describe Board do
 
     context '#initialize' do
-      before(:all) @board = Board.new
+      before(:all) { @board = Board.new }
 
       it 'initializes with two rows' do
         expect(@board.grid.size).to eq 2
       end
-      
+
     end
   end
 end
