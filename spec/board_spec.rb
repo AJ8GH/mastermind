6aa7a611d1a44f2ee43ad35c3215c89f2021a13c
Adam Jonas <adamjonas25@gmail.com>
2021-01-19 21:23:34 +0000

    pass spec for grid getter method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,8 @@
 module Mastermind
   describe Board do
+    before(:all) { @board = Board.new }
 
     context '#initialize' do
-      before(:all) { @board = Board.new }
-
       it 'initializes with two rows' do
         expect(@board.grid.size).to eq 2
       end
@@ -14,6 +13,13 @@ module Mastermind
 
       it 'consitists of cell objects' do
         expect(@board.grid.all? { |row| row.all? { |cell| cell.class == Cell } }).to be true
+      end
+    end
+
+    context '#grid' do
+      it 'returns the grid' do
+        expect(@board.grid.map { |row| row.map(&:color) }).to eq [['', '', '', ''],
+                                                                  ['', '', '', '']]
       end
     end
   end
