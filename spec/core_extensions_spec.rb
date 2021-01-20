a0de453545c036a2010c9ccce300073299257d6b
Adam Jonas <adamjonas25@gmail.com>
2021-01-20 18:36:54 +0000

    pass spec for stdout on underline method

--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -1,4 +1,6 @@
 describe String do
+
+
   context '#centralize' do
     it 'centralizes a string within 80 characters' do
       expect(''.centralize).to eq ''.center(80)
@@ -8,6 +10,26 @@ describe String do
   context '#line' do
     it 'returns a line of 80 hyphens' do
       expect(''.line).to eq('-' * 80)
+    end
+  end
+
+  context '#underline' do
+    let(:expected_output) { 'a'.center(80) + "\n" + ("-" * 80) + "\n" }
+
+    it 'puts self.centralize then puts line' do
+      expect { 'a'.underline }.to output(expected_output).to_stdout
+    end
+  end
+
+  context '#overline' do
+    it '' do
+
+    end
+  end
+
+  context '#over_under_line' do
+    it '' do
+
     end
   end
 end
