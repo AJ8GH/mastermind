046c0893a05011c6bc266b81c527a97cb3b3251e
Adam Jonas <adamjonas25@gmail.com>
2021-01-20 12:29:12 +0000

    define get player names and begin new game methods

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -2,3 +2,19 @@ require_relative 'player'
 require_relative 'cell'
 require_relative 'board'
 require_relative 'game'
+
+def get_player_names
+  puts "Player one, enter your name:"
+  player_one = gets.chomp
+
+  puts "Player two, enter your name:"
+  player_two = gets.chomp
+
+  @players = [player_one, player_two]
+end
+
+def begin_new_game
+  get_player_names
+  new_game = Game.new(@players)
+  new_game.play_game
+end
